fbcfbcbd5607c55c2b89cde85be4e2cb98c75c3d
Richardson Lima <contatorichardsonlima@gmail.com>
2021-09-06 23:40:26 -0300

    Update call_create_vm.sh

--- scripts/kvm-equinix-metalbox/k8s-provisioner-on-kvm/call_create_vm.sh
+++ scripts/kvm-equinix-metalbox/k8s-provisioner-on-kvm/call_create_vm.sh
@@ -1,5 +1,5 @@
 export LIBVIRT_DEFAULT_URI="qemu:///system"
-for n in $(seq 1 4); do
+for n in $(seq 1 3); do
     ./create-vm.sh -n node$n \
       -i 'http://archive.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/' \
       -k ./ubuntu.ks \
