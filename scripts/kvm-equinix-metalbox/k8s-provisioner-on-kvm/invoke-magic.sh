b0d8e83dd4d818caa39ef38c120625405ccab907
Richardson Lima <contatorichardsonlima@gmail.com>
2021-09-22 23:59:45 -0300

    fix

--- scripts/kvm-equinix-metalbox/k8s-provisioner-on-kvm/invoke-magic.sh
+++ scripts/kvm-equinix-metalbox/k8s-provisioner-on-kvm/invoke-magic.sh
@@ -1,5 +1,4 @@
-CONFIG_FILE=/home/jenkins/var/jenkins_home/workspace/baremetal-create-k8s-cluster/inventory/mycluster/hosts.yml 
-
+export CONFIG_FILE=/home/jenkins/var/jenkins_home/workspace/baremetal-create-k8s-cluster/inventory/mycluster/hosts.yml
 declare -a IPS=($(for n in $(seq 1 3); do sudo ./get-vm-ip.sh node$n; done))
-echo ${IPS[@]}
+#echo ${IPS[@]}
 python3 /home/jenkins/var/jenkins_home/workspace/baremetal-create-k8s-cluster/contrib/inventory_builder/inventory.py ${IPS[@]}
