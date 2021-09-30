158d272ab5abfb8c16961f2bb47b7b055b579415
Richardson Lima <contatorichardsonlima@gmail.com>
2021-09-29 23:44:17 -0300

    fix

--- scripts/kvm-equinix-metalbox/k8s-provisioner-on-kvm/invoke-magic.sh
+++ scripts/kvm-equinix-metalbox/k8s-provisioner-on-kvm/invoke-magic.sh
@@ -1,4 +1,4 @@
-export CONFIG_FILE=$(pwd)/inventory/mycluster/hosts.yml
+export CONFIG_FILE=/home/jenkins/var/jenkins_home/workspace/pipeline-only-for-debug/inventory/mycluster/hosts.yml
 declare -a IPS=($(for n in $(seq 1 3); do sudo ./get-vm-ip.sh node$n; done))
 #echo ${IPS[@]}
 python3 ./inventory.py ${IPS[@]}
