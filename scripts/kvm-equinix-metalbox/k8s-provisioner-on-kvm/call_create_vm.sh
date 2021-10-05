ce8517c7cf04f2c9a243815e235c4e3b8338bd4b
Richardson Lima <contatorichardsonlima@gmail.com>
2021-10-05 18:43:49 -0300

    Update call_create_vm.sh

--- scripts/kvm-equinix-metalbox/k8s-provisioner-on-kvm/call_create_vm.sh
+++ scripts/kvm-equinix-metalbox/k8s-provisioner-on-kvm/call_create_vm.sh
@@ -3,7 +3,7 @@ for n in $(seq 1 3); do
     ./create-vm.sh -n node$n \
       -i 'http://archive.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/' \
       -k ./ubuntu.ks \
-      -r 512 \
-      -c 1 \
+      -r 2048 \
+      -c 8 \
       -s 15
 done
