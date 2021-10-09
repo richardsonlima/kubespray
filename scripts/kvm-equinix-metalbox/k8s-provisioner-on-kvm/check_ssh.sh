44870f0d177c109e3f730873ec2e6812d7920c54
Richardson Lima <contatorichardsonlima@gmail.com>
2021-10-09 02:05:57 -0300

    fix

--- scripts/kvm-equinix-metalbox/k8s-provisioner-on-kvm/check_ssh.sh
+++ scripts/kvm-equinix-metalbox/k8s-provisioner-on-kvm/check_ssh.sh
@@ -1,1 +1,13 @@
-while ! ssh -oStrictHostKeyChecking=no -t -i ./id_rsa ansible@$(sudo bash ./get-vm-ip.sh node1); do echo "Checking if SSH is UP! Trying again..."; done
+for node in $(sudo virsh list --all|awk '{print $2}'|grep -v Name); do
+tmp=$(sudo virsh list --all | grep " $node " | awk '{ print $3}')
+if ([ "x$tmp" == "x" ] || [ "x$tmp" != "xrunning" ])
+then
+    echo "$node does not exist or is shut down!"
+    sudo bash -x ./check_and_start_vm.sh && sleep 60
+    while ! ssh -oStrictHostKeyChecking=no -t -i ./id_rsa ansible@$(sudo bash ./get-vm-ip.sh $node); do echo "Checking if SSH is UP! Trying again..."; done
+else
+    echo "$node is running!"
+    echo "$node Checking if SSH is UP!"
+    while ! ssh -oStrictHostKeyChecking=no -t -i ./id_rsa ansible@$(sudo bash ./get-vm-ip.sh $node); do echo "Checking if SSH is UP! Trying again..."; done
+fi
+done
